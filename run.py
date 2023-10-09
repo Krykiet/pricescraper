556213b0e0b19b95d7a83b75a548733e9cb95e50
Krystian Chudzik <kchudzik@emacs.local>
2023-10-09 14:30:49 +0200

    changed prod port and host

--- run.py
+++ run.py
@@ -4,4 +4,10 @@ from app.config import get_config
 
 if __name__ == "__main__":
     config = get_config()
-    uvicorn.run("app.main:app", reload=config.local)
+    if config.local:
+        print('@@Config local@@')
+        uvicorn.run("app.main:app", reload=config.local)
+    else:
+        print('@@Config prod@@')
+        uvicorn.run("app.main:app", port=10000, host='0.0.0.0', reload=config.prod)
+
