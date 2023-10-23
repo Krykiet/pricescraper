10d3704a867aeb0d15dca9ac54fa90d48666c57e
Krystian Chudzik <kchudzik@emacs.local>
2023-10-23 12:17:49 +0200

    deleted 'User-agent' header

--- app/services/scraper.py
+++ app/services/scraper.py
@@ -10,7 +10,6 @@ SCRAPER_MESSAGE = 'Scraped!'
 
 def scrape_rdn():
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Cache-Control': 'no-cache',
         'Pragma': 'no-cache',
         # existing headers...
